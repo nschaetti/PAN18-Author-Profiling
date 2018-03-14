184b593873023525f3bc39507909c81ad2988c4a
Nils Schaetti <n.schaetti@gmail.com>
2018-03-14 19:41:12 +0100

    Image dataset

--- dataset/ImageDataset.py
+++ dataset/ImageDataset.py
@@ -92,7 +92,9 @@ class ImageDataset(Dataset):
         current_image_path = self.images[item]
 
         # Corresponding ID
-        idx = current_image_path[:-6]
+        idx = current_image_path[current_image_path.find('/')+1:-7]
+        print(idx)
+        exit()
 
         # PIL image
         try:
