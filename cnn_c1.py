26a7dda04ef8c041870d7206e22998213ac45f27
Nils Schaetti <n.schaetti@gmail.com>
2018-03-14 15:39:46 +0100

    Transformed

--- cnn_c1.py
+++ cnn_c1.py
@@ -10,17 +10,19 @@ from echotorch.transforms import text
 # Settings
 batch_size = 1
 
+# Text tranformer
+text_transform = text.Character()
+
 # Author profiling training data sets
 pan18loader_training = torch.utils.data.DataLoader(
-    dataset.AuthorProfilingDataset(root="./data/", download=True),
+    dataset.AuthorProfilingDataset(root="./data/", download=True, text_transform=text_transform),
     batch_size=batch_size, shuffle=True)
 
 # For each samples
 for data in pan18loader_training:
     # Inputs and labels
-    tweets, images, label = data
+    tweets, label = data
     print(tweets)
-    print(images)
     print(label)
     print(u"")
 # end for
