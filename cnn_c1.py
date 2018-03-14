269e3d1f83290bc67536764d1e9f4d41f23bec48
Nils Schaetti <n.schaetti@gmail.com>
2018-03-14 16:54:30 +0100

    CNN-C1

--- cnn_c1.py
+++ cnn_c1.py
@@ -3,19 +3,29 @@
 
 # Imports
 import torch
+from torchvision import transforms
 import dataset
 from echotorch.transforms import text
 
 
 # Settings
 batch_size = 1
+image_size = 300
 
 # Text tranformer
 text_transform = text.Character()
 
+# Image transformer
+image_transform = transforms.Compose([
+        transforms.CenterCrop(image_size),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+
 # Author profiling training data sets
 pan18loader_training = torch.utils.data.DataLoader(
-    dataset.AuthorProfilingDataset(min_length=165, root="./data/", download=True, text_transform=text_transform),
+    dataset.AuthorProfilingDataset(min_length=165, root="./data/", download=True, text_transform=text_transform,
+                                   image_transform=image_transform, image_size=image_size),
     batch_size=batch_size, shuffle=True)
 
 # For each samples
