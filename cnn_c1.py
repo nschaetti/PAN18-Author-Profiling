a637c49e98f2f9b3993507243eb00043266d7284
nschaetti <n.schaetti@gmail.com>
2018-03-14 11:34:33 +0100

    CNN-C1

---
+++ cnn_c1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#
+
+# Imports
+import torch
+import dataset
+from echotorch.transforms import text
+
+
+# Settings
+batch_size = 64
+
+# Author profiling training data sets
+pan18loader_training = torch.utils.data.DataLoader(
+    dataset.AuthorIdentificationDataset(root="./data/", download=True),
+    batch_size=batch_size, shuffle=True)
+
+# For each samples
+"""for data in pan18loader_training:
+    # Inputs and labels
+    inputs, images, labels = data
+    print(inputs.size())
+    print(labels.size())
+# end for"""
