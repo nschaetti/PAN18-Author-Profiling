820fd725a29f12fb607d5eb1f4570b10ad0c8e40
nschaetti <n.schaetti@gmail.com>
2018-04-12 10:11:07 +0200

    CNN C2

--- cnn_c2.py
+++ cnn_c2.py
@@ -3,57 +3,61 @@
 
 # Imports
 import torch
-from torchvision import transforms
 import dataset
-from echotorch.transforms import text
+from torchlanguage import transforms
+from torchlanguage import models
 import argparse
 import torch.nn as nn
-from modules import CNNT
 from torch.autograd import Variable
 from torch import optim
 import copy
+import os
 
 
 # Settings
-batch_size = 5
-image_size = 100
+batch_size = 30
 min_length = 165
-n_epoch = 1
-voc_size = 1000
+voc_size = 20311
 
 # Argument parser
-parser = argparse.ArgumentParser(description="PAN18 Author Profiling CNN-C1")
+parser = argparse.ArgumentParser(description="PAN18 Author Profiling CNN-C2")
 
 # Argument
 parser.add_argument("--output", type=str, help="Model output file", default='.')
-parser.add_argument("--dim", type=int, help="Embedding dimension", default=300)
+parser.add_argument("--dim", type=int, help="Embedding dimension", default=50)
 parser.add_argument("--no-cuda", action='store_true', default=False, help="Enables CUDA training")
 parser.add_argument("--epoch", type=int, help="Epoch", default=300)
+parser.add_argument("--lang", type=str, help="Language", default='en')
 args = parser.parse_args()
 
 # Use CUDA?
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 
 # Text tranformer
-text_transform = text.Character2Gram()
+text_transform = transforms.Compose([
+    transforms.RemoveRegex(regex=r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'),
+    transforms.ToLower(),
+    transforms.Character2Gram(),
+    transforms.ToIndex(start_ix=1)
+])
 
 # Tweet data set 2017 training
-tweet_dataset_train_17 = dataset.TweetDataset(min_length=min_length, root='./data/', download=True,
+tweet_dataset_train_17 = dataset.TweetDataset(min_length=min_length, root='./data/', download=True, lang=args.lang,
                                               text_transform=text_transform, year=2017, train=True)
 pan17loader_training = torch.utils.data.DataLoader(tweet_dataset_train_17, batch_size=batch_size, shuffle=True)
 
 # Tweet data set 2017 validation
-tweet_dataset_val_17 = dataset.TweetDataset(min_length=min_length, root='./data/', download=True,
+tweet_dataset_val_17 = dataset.TweetDataset(min_length=min_length, root='./data/', download=True, lang=args.lang,
                                             text_transform=text_transform, year=2017, train=False)
 pan17loader_validation = torch.utils.data.DataLoader(tweet_dataset_val_17, batch_size=batch_size, shuffle=True)
 
 # Tweet data set 2018 training
-tweet_dataset_train_18 = dataset.TweetDataset(min_length=min_length, root='./data/', download=True,
+tweet_dataset_train_18 = dataset.TweetDataset(min_length=min_length, root='./data/', download=True, lang=args.lang,
                                               text_transform=text_transform, year=2018, train=True)
 pan18loader_training = torch.utils.data.DataLoader(tweet_dataset_train_18, batch_size=batch_size, shuffle=True)
 
 # Tweet data set 2018 validation
-tweet_dataset_val_18 = dataset.TweetDataset(min_length=min_length, root='./data/', download=True,
+tweet_dataset_val_18 = dataset.TweetDataset(min_length=min_length, root='./data/', download=True, lang=args.lang,
                                             text_transform=text_transform, year=2018, train=False)
 pan18loader_validation = torch.utils.data.DataLoader(tweet_dataset_val_18, batch_size=batch_size, shuffle=True)
 
@@ -61,7 +65,7 @@ pan18loader_validation = torch.utils.data.DataLoader(tweet_dataset_val_18, batch
 loss_function = nn.CrossEntropyLoss()
 
 # Model
-model = CNNT(vocab_size=voc_size, embedding_dim=args.dim)
+model = models.CNNCTweet(text_length=min_length, vocab_size=voc_size, embedding_dim=args.dim)
 if args.cuda:
     model.cuda()
 # end if
@@ -72,7 +76,7 @@ best_acc = 0.0
 optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
 # Epoch
-for epoch in range(n_epoch):
+for epoch in range(args.epoch):
     # Total losses
     training_loss = 0.0
     training_total = 0.0
@@ -169,4 +173,5 @@ for epoch in range(n_epoch):
 model.load_state_dict(best_model)
 
 # Save
-torch.save((text_transform.gram_to_ix, model), open(args.output, 'wb'))
+torch.save(text_transform.transforms[2].token_to_ix, open(os.path.join(args.output, "voc.p"), 'wb'))
+torch.save(model, open(os.path.join(args.output, "model.p"), 'wb'))
