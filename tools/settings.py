5801aff5d39253c1425a18dc601bc1d1427b656d
nschaetti <n.schaetti@gmail.com>
2018-05-03 10:39:43 +0200

    Removed alpha

--- tools/settings.py
+++ tools/settings.py
@@ -16,7 +16,6 @@ min_length = 165
 voc_sizes = {'c1': {'en': 1628, 'ar': 1839, 'es': 1805}, 'c2': {'en': 21510, 'ar': 31694, 'es': 30025}}
 class_to_idx = {'female': 0, 'male': 1}
 idx_to_class = {0: 'female', 1: 'male'}
-alpha = 0.1
 
 # Image models
 image_models = {
