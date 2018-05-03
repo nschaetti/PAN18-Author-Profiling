b4bd2d3fd4af7958764c2951fd98ff14ef806ee1
nschaetti <n.schaetti@gmail.com>
2018-05-03 10:39:04 +0200

    Image model URL

--- tools/settings.py
+++ tools/settings.py
@@ -16,4 +16,10 @@ min_length = 165
 voc_sizes = {'c1': {'en': 1628, 'ar': 1839, 'es': 1805}, 'c2': {'en': 21510, 'ar': 31694, 'es': 30025}}
 class_to_idx = {'female': 0, 'male': 1}
 idx_to_class = {0: 'female', 1: 'male'}
-alpha = 0.5
+alpha = 0.1
+
+# Image models
+image_models = {
+    'resnet18': 'https://www.nilsschaetti.com/models/resnet18-d2fa3a7e.pth',
+    'alexnet': 'https://www.nilsschaetti.com/models/alexnet-bd63e232.pth'
+}
